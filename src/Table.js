46bee9c4c96ef30b25ff7e766d4c3402ce1c93fb
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-03-03 14:57:57 +0900

    props....

--- src/Table.js
+++ src/Table.js
@@ -1,33 +1,44 @@
 import React, { Component } from "react";
 
+const TableHeader = () => {
+  return (
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Job</th>
+      </tr>
+    </thead>
+  );
+};
+
+const TableBody = () => {
+  return <tbody />;
+};
+
 class Table extends Component {
   render() {
+    const characters = [
+      {
+        name: "Charlie",
+        job: "Janitor",
+      },
+      {
+        name: "Mac",
+        job: "Bouncer",
+      },
+      {
+        name: "Dee",
+        job: "Aspring actress",
+      },
+      {
+        name: "Dennis",
+        job: "Bartender",
+      },
+    ];
     return (
       <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Job</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Charlie</td>
-            <td>Janitor</td>
-          </tr>
-          <tr>
-            <td>Mac</td>
-            <td>Bouncer</td>
-          </tr>
-          <tr>
-            <td>Dee</td>
-            <td>Aspiring actress</td>
-          </tr>
-          <tr>
-            <td>Dennis</td>
-            <td>Bartender</td>
-          </tr>
-        </tbody>
+        <TableHeader />
+        <TableBody characterData={characters} />
       </table>
     );
   }
